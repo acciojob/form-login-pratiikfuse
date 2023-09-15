289696bb9f0880412c0dcf1b9443d9c79bd1c927
Pratik Fuse <pratikfuse20@gmail.com>
2023-09-15 14:02:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
 function getFormvalue() {
     //Write your code here
+	let form1 = document.getElementById('form1');
+	let childrens = form1.children;
+
+	let firstname = childrens[0].value;
+	let secondname = childerns[0].value;
+	alert(firstname+" "+lastname);
 
 }
