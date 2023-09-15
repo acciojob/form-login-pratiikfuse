a4b1adee63a1cd6728c9540d8a030f728ccba129
Pratik Fuse <pratikfuse20@gmail.com>
2023-09-15 14:14:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 function getFormvalue() {
     //Write your code here
 	let form1 = document.getElementById('form1');
-	let childrens = form1.children;
+	let childrens = form1.childNodes;
 
 	let firstname = childrens[0].value;
 	let secondname = childrens[1].value;
